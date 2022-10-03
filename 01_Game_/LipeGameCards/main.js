c86a862713c4ab986487b71d0ddb2783dae51159
luizf <luizfilipe.limaxavier@gmail.com>
2022-10-03 19:15:06 -0300

    Work progress - create component wothout css

--- 01_Game_/LipeGameCards/main.js
+++ 01_Game_/LipeGameCards/main.js
@@ -3,11 +3,16 @@ import"./src/styles/settings/colors.css";
 import "./src/styles/generic/reset.css";
 import"./src/styles/elements/base.css";
 import BoardGame from "./src/objects/BoardGame";
+import PlayerName from "./src/components/PlayerName";
 
 //Boa pratica: iniciar variavel com $ quando for uma referencia da tela.
 const $root = document.querySelector("#root");
-//Parametro BoardGame=> Quantidade de cartas na mesa
-const $htmlBoardGame = BoardGame(6);
 
-$root.insertAdjacentHTML("beforeend", $htmlBoardGame);
+$root.insertAdjacentHTML(
+    "beforeend", `
+    ${PlayerName()}
+    ${PlayerName()}
+    ${BoardGame(6)}
+    `
+);
 
