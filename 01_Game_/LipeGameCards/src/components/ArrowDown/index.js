6d584e76736bcef8220063c1d976c87fcca44e58
luizf <luizfilipe.limaxavier@gmail.com>
2022-10-04 08:11:47 -0300

    Create ArrowDown component without css

---
+++ 01_Game_/LipeGameCards/src/components/ArrowDown/index.js
@@ -0,0 +1,12 @@
+import "./style.css";
+
+function ArrowDown() {
+    return /*html*/ `
+        
+        <img src="images/IconArrowDown.svg" 
+        alt="Icone de seta indicativa do player">
+                
+    `;
+}
+
+export default ArrowDown;
