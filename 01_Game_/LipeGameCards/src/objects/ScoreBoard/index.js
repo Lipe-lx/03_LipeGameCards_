23e28dde852dd8db4439e445d0e72b33811042e4
luizf <luizfilipe.limaxavier@gmail.com>
2022-10-04 06:15:32 -0300

    Creat html of the PlayerScore component

--- 01_Game_/LipeGameCards/src/objects/ScoreBoard/index.js
+++ 01_Game_/LipeGameCards/src/objects/ScoreBoard/index.js
@@ -1,4 +1,5 @@
 import PlayerName from "../../components/PlayerName";
+import PlayerScore from "../../components/PlayerScore";
 import VsPlayer from "../../components/VsPlayer";
 import "./style.css";
 
@@ -7,7 +8,9 @@ function ScoreBoard(){
     return /*html*/`
         <header class="score-board">
             ${PlayerName()}
+            ${PlayerScore(1)}
             ${VsPlayer()}
+            ${PlayerScore()}
             ${PlayerName()}
         </header>
     `;
