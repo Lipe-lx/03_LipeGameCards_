e967e1afc15eb16b1c9d4902cfd65c26cfc30c73
luizf <luizfilipe.limaxavier@gmail.com>
2022-10-04 10:35:41 -0300

    change player when flip to cards

--- 01_Game_/LipeGameCards/src/objects/BoardGame/index.js
+++ 01_Game_/LipeGameCards/src/objects/BoardGame/index.js
@@ -4,16 +4,30 @@ import CardFrontBack from "../../components/CardFrontBack";
 import cards from "./data";
 
 function BoardGame(amountCards) {
-    //Utilização do metodo repeat para multiplicar as cartas
-    //const $htmlContentBoardGame = $htmlCardFrontBack.repeat(amountCards);
-    
-    
+    window.boardGame = {};
+    window.boardGame.handleClick = () => {
+        const $boardGame = document.querySelector(".board-game");
+        //setinha indicativa do player
+        const $arrowDown = document.querySelector(".arrow-down");
+        const $cardsActive = $boardGame.querySelectorAll(".card-front-back.-active");
+        
+        //Aplicação da regra:Quando tivermos duas cartas active (viradas) as mesmas sao desativadas, espera de 1000ms = 1s.
+        if ($cardsActive.length == 2) {
+            setTimeout(() => {
+                const currentPlayer = $arrowDown.getAttribute("data-currentPlayer")
+                //Esconde as cartas
+                $cardsActive.forEach((card) => card.classList.remove("-active"));
+                //Mudanca de jogador: se currentPlayer for igual a 1 entao muda para 2 se nao muda para 1
+                $arrowDown.setAttribute("data-currentPlayer", currentPlayer == 1 ? 2 : 1);
+            }, 600);
+        }
+    }
     //criando um mapa para passarmos por toda a lista de cartas disponiveis
     const $htmlCardsList = cards.map((card) => CardFrontBack(card.icon, card.altIcon));
     const $htmlCards = $htmlCardsList.join("")
 
     return /*html*/`
-        <section class="board-game">
+        <section class="board-game" onClick="boardGame.handleClick()">
             ${$htmlCards}
         </section>
     `;
